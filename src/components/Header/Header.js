06c8bbe9f31ea7fd8bad904da4f556cce651137a
Daniel Fosco <daniel.fosco@vtex.com.br>
2015-10-16 14:33:25 -0300

    Fix header alignment and column sizes Adjust header style to fit correct class naming Adjust ProductHeader to fit properly the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,21 +9,21 @@ import cartIcon from 'assets/icons/cart.svg';
 class Header extends React.Component {
   render() {
     return (
-      <div className="v-header clearfix">
+      <div className="Header clearfix">
         <div className="col-xs-1">
-          <SVGIcon className="v-header__icon" svg={hamburgerIcon} width={18} height={18} fill="#153243"/>
+          <SVGIcon className="icon" svg={hamburgerIcon} width={18} height={18} fill="#153243"/>
         </div>
 
-        <h1 className="v-header__brand col-xs-8">
+        <h1 className="brand col-xs-8 col-xs-push-1">
           <Link to="/">Fera fashion</Link>
         </h1>
 
         <div className="col-xs-1">
-          <SVGIcon className="v-header__icon" svg={searchIcon} width={15} height={18} fill="#153243"/>
+          <SVGIcon className="icon" svg={searchIcon} width={15} height={18} fill="#153243"/>
         </div>
 
-        <div className="col-xs-2">
-          <SVGIcon className="v-header__icon" svg={cartIcon} width={18} height={18} fill="#153243"/>
+        <div className="col-xs-1">
+          <SVGIcon className="icon" svg={cartIcon} width={18} height={18} fill="#153243"/>
         </div>
       </div>
     );
