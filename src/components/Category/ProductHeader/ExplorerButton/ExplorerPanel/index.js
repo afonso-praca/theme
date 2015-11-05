3fbcdb0e58b4f926b1991af3554bd0324207aecb
Daniel Fosco <daniel.fosco@vtex.com.br>
2015-11-04 22:39:37 -0200

    Finish current iteration for advanced category navigation

--- src/components/Category/ProductHeader/ExplorerButton/ExplorerPanel/index.js
+++ src/components/Category/ProductHeader/ExplorerButton/ExplorerPanel/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './style.less';
 import PanelNav from 'components/Category/common/PanelNav';
 import CategoryList from './CategoryList';
+import SVGIcon from 'utils/SVGIcon';
+import pullArrowIcon from 'assets/icons/pullArrow_icon.svg';
+import pullArrowImg from 'assets/icons/pullArrow_icon.png';
+
 
 class ExplorerPanel extends React.Component {
   render() {
@@ -9,12 +13,21 @@ class ExplorerPanel extends React.Component {
 
     return (
       <aside className="ExplorerPanel" data-is-open={isOpen}>
-        <PanelNav closePanel={this.props.closeExplorerPanel} navType='xbar' />
-        <div className="ExplorerPanel-header clearfix container">
-          <h1 className="ExplorerPanel-title">Blusas</h1>
-          <span className="ExplorerPanel-subtitle">Subcategorias</span>
+        <div className="ExplorerPanel-content clearfix">
+          <PanelNav closePanel={this.props.closeExplorerPanel} navType='xbar' />
+          <div className="ExplorerPanel-header clearfix container">
+            <h1 className="ExplorerPanel-title">Blusas</h1>
+            <span className="ExplorerPanel-subtitle">Subcategorias</span>
+          </div>
+          <CategoryList />
+          <button className="ExplorerPanel-pull-button col-xs-2 col-xs-push-5">
+            <SVGIcon className="CategoryOption-icon"
+                       svg={pullArrowIcon}
+                       fallback={pullArrowImg} width={30}
+                       cleanupExceptions={['width', 'height']}
+                       fill='#5C6F7F' />
+          </button>
         </div>
-        <CategoryList />
       </aside>
     );
   }
