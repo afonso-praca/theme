c3ac88dbb77c6ba0f910f886568902b44045468b
Daniel Fosco <daniel.fosco@vtex.com.br>
2015-11-11 17:28:36 -0200

    Improve structure and refactor classnames

--- src/components/Category/ProductHeader/index.js
+++ src/components/Category/ProductHeader/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './style.less';
-//import CategoryButton from './CategoryButton';
-//import OrderButton from './OrderButton';
 import FilterButton from './FilterButton';
 import ExibitionButton from './ExibitionButton';
 
@@ -11,21 +9,19 @@ class ProductHeader extends React.Component {
     let resultNumber = 25;
     return (
       <nav className="ProductHeader container">
-        <div className="header-container">
-          <div className="row header-content">
-            <div className="categoryTitle">
-              <h2 className="header-title">
-                { this.props.category.get('name') }
-              </h2>
-            </div>
+        <div className="ProductHeader-header-container">
+          <div className="row ProductHeader-content">
             <div className="col-xs-12">
-              <span className="header-results">
-                Sua busca trouxe {resultNumber} resultados
+              <span className="ProductHeader-results">
+                {resultNumber} resultados para:
               </span>
             </div>
+            <h2 className="ProductHeader-search-title">
+              { this.props.category.get('name') }
+            </h2>
           </div>
           <div className="row">
-            <div className="header-buttons">
+            <div className="ProductHeader-buttons">
               <FilterButton className="col-xs-10" />
               <ExibitionButton className="col-xs-2" changeExibitionMode={this.props.changeExibitionMode} />
             </div>
