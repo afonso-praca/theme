f2464333b57783ad1a1739853c54ac1b40aa46aa
Gabriel <gabriel.kozlowaski@vtex.com.br>
2016-04-01 18:51:11 -0300

    Implements loading

--- src/components/DefaultTemplate/DefaultTemplate.js
+++ src/components/DefaultTemplate/DefaultTemplate.js
@@ -2,14 +2,41 @@ import React from 'react';
 import Header from 'components/Header';
 import Footer from 'components/Footer/Footer';
 import Newsletter from 'components/Newsletter/Newsletter';
+import './spinner.less';
+import './DefaultTemplate.less'
 import { stores } from 'sdk';
 
 const Placeholder = stores.ComponentStore.state.getIn(['Placeholder@vtex.storefront-sdk', 'constructor']);
 
 class DefaultTemplate extends React.Component {
+
+  componentWillMount() {
+    this.setState({ loading: false });
+
+    stores.ContextStore.listen(this.onChange);
+  }
+
+  componentWillUnmount() {
+    stores.ContextStore.unlisten(this.onChange);
+  }
+
+  onChange = (ContextStore) => {
+    this.setState({ loading: ContextStore.get('loading') });
+  }
+
+
   render() {
+    const loading = this.state.loading ?
+      <div className="DefaultTemplate__spin-wrap">
+        <div className="spinner">
+          <div className="dot1"></div>
+          <div className="dot2"></div>
+        </div>
+      </div> : null;
+
     return (
       <div className="DefaultTemplate">
+        { loading }
         <Header />
           <Placeholder
             id="body"
