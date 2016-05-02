e2333db438b5d39a83278487e98063ff737b0baa
Daniel Fosco <daniel.fosco@vtex.com.br>
2016-05-02 20:21:16 -0300

    Add ProductNotFound component

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './ProductPage.less';
 import Product from 'components/Product/Product';
+import ProductNotFound from './ProductNotFound/ProductNotFound.js';
 
 class ProductPage extends React.Component {
   render() {
     return (
       <div className="ProductPage">
         <div className="ProductPage__wrapper container-fluid">
-          {this.props.loading ? <div>Carregando</div> : <Product {...this.props.product}/>}
+          {this.props.loading ? <ProductNotFound/> : <Product {...this.props.product}/>}
         </div>
       </div>
     );
